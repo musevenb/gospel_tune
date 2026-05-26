451ac004a5ec02f111288df3d67b2d65b85fedf9
musevenb <127434265+musevenb@users.noreply.github.com>
2026-05-27 00:45:35 +0200

    Configure build options and chunking in Vite

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,22 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit', 'react-redux']
   }
 })
